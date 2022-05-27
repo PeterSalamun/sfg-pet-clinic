ec5ee4d9aa0cb682a58bcbc03403acbaeb3764de
Peter Salamun <peter.salamun@gmail.com>
2022-05-27 11:25:50 +0200

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/service/VetService.java
@@ -0,0 +1,16 @@
+package com.example.sfgpetclinic.service;
+
+import com.example.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+
+}
+
