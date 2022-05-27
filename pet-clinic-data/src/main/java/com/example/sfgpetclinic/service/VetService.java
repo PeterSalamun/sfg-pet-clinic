e43bbc57c922df8760425fe2f2cdf07945d9b621
Peter Salamun <peter.salamun@gmail.com>
2022-05-27 14:41:45 +0200

    Refactor to common base interface

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/service/VetService.java
@@ -2,15 +2,7 @@ package com.example.sfgpetclinic.service;
 
 import com.example.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 
 }
 
