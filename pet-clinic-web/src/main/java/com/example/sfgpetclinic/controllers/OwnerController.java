9deb01250b76951b49b9717bc0b521be074f31e4
Peter Salamun <peter.salamun@gmail.com>
2022-05-30 13:40:49 +0200

    add index page and Controller for Owners, close #12

--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/OwnerController.java
@@ -1,12 +1,14 @@
 package com.example.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @GetMapping({"", "/", "/index", "/index.html"})
     public String getOwnersList() {
         return "owners/index";
     }
