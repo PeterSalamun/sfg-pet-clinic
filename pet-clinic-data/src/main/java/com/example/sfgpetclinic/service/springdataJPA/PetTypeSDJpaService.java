cf406e62a3906b482a0214eefaacf290b63440a4
Peter Salamun <peter.salamun@gmail.com>
2022-06-15 09:01:01 +0200

    Restore the services for Pet, PetType and Specialty.

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/service/springdataJPA/PetTypeSDJpaService.java
@@ -0,0 +1,46 @@
+package com.example.sfgpetclinic.service.springdataJPA;
+
+import com.example.sfgpetclinic.model.PetType;
+import com.example.sfgpetclinic.repositories.PetTypeRepository;
+import com.example.sfgpetclinic.service.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdata")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return new HashSet<>(petTypeRepository.findAll());
+    }
+
+    @Override
+    public PetType findById(Long petTypeId) {
+        return petTypeRepository.findById(petTypeId).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petTypeRepository.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        petTypeRepository.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long petTypeId) {
+        petTypeRepository.deleteById(petTypeId);
+    }
+}
